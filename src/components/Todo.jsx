f681951bbd1533c3d5211383dbe8cb0e437f2805
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-07 00:15:56 +0500

    React Todo App

---
+++ src/components/Todo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Todo = ({ todo }) => {
+  return <div className="todoS__t">{todo}</div>;
+};
+
+export default Todo;
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,12 +1,43 @@
 import React, { useState } from "react";
 import "./todos.css";
-
+import Todo from "./Todo";
+let todoC = [];
 const Todos = () => {
-  const [todo, setTodo] = useState([]);
+  const [todo, setTodo] = useState(todoC);
+  const [value, setValue] = useState("");
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    todoC.push(value);
+    setTodo(todoC);
+    clear();
+  };
+  const handleDelete = (i) => {
+    todoC.splice(i, 1);
+    setTodo(todoC);
+    clear();
+  };
+  const clear = () => {
+    setValue("");
+  };
+
   return (
     <div className="todo__">
       <div className="todo__input">
-        <input type="text" placeholder="Add todo" />
+        <input
+          type="text"
+          placeholder="Add todo"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button onClick={handleAdd}> Add </button>
+      </div>
+      <div className="todo__todos">
+        {todo.map((t, i) => (
+          <div key={i} onClick={() => handleDelete(i)}>
+            <Todo todo={t} todoValues={todoC} handleAdd={handleAdd} />
+          </div>
+        ))}
       </div>
     </div>
   );
